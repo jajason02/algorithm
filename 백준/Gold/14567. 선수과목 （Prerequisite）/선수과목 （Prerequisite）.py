153f5064961aa4828e9e25f43aa7ca45fc19a5fa
Jaehyeon Park <zizon1818@gmail.com>
2026-03-29 16:36:04 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 12772 ms, Memory: 51376 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
@@ -1,5 +1,9 @@
 """
 나의 생각: 위상정렬 큐로 풀어버린다 이번엔 절대로 안틀린다.
+ai 도움: visited 없애고 while 탐색 안에서 degree가 0이 되는 순간,
+바로 queue에 넣어버리기
+for neighbor in (node[idx]): 에서 len(node[idx]) 쓰지말고 node[idx]로 실행
+리스트 언패킹 print(*(semester[1:]))
 """
 
 from collections import deque
@@ -8,8 +12,9 @@ N, M = map(int, input().split())
 node = [[] for _ in range(N + 1)]
 degree = [0] * (N + 1)
 semester = [0] * (N + 1)
-visited = [0] * (N + 1)
-visited[0] = 1
+
+# visited = [0] * (N + 1)
+# visited[0] = 1
 
 for _ in range(M):
     u, v = map(int, input().split())
@@ -26,14 +31,17 @@ semester_idx = 1
 while queue:
     for _ in range (len(queue)):
         idx = queue.popleft()
-        visited[idx] = 1
-        for n in range(len(node[idx])):
-            degree[node[idx][n]] -= 1
+        # visited[idx] = 1
+        for neighbor in (node[idx]):
+            degree[neighbor] -= 1
+            if degree[neighbor] == 0:
+                queue.append(neighbor)
         semester[idx] = semester_idx
     
-    for n in range(1, N + 1):
-        if not visited[n] and degree[n] == 0:
-            queue.append(n)
+    # for n in range(1, N + 1):
+    #     if not visited[n] and degree[n] == 0:
+    #         queue.append(n)
     semester_idx += 1
-for n in range(1, N + 1):
-    print(semester[n],end=" ")
+# for neighbor in range(1, N + 1):
+#     print(semester[neighbor],end=" ")
+print(*(semester[1:]))
